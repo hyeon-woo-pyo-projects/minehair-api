156ab1bdfe6afd1e7b12d8f3bbb9ce7ea3f6602f
woopi1030 <lozenkrantz@gmail.com>
2025-07-20 20:07:51 +0900

    [ADD] plugin.jpa 추가 및 버전 관리 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
 	kotlin("jvm") version "1.9.25"
 	kotlin("plugin.spring") version "1.9.25"
+	kotlin("plugin.jpa") version "1.9.25"
 	id("org.springframework.boot") version "3.5.3"
 	id("io.spring.dependency-management") version "1.1.7"
 }
@@ -24,16 +25,22 @@ repositories {
 	mavenCentral()
 }
 
+val commonsLangVersion = "3.18.0"
+val springdocVersion = "2.8.9"
+
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 
+	// Apache Commons Lang
+	implementation("org.apache.commons:commons-lang3:$commonsLangVersion")
+
 	// JPA
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
 	// Swagger
-	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.9")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$springdocVersion")
 
 	compileOnly("org.projectlombok:lombok")
 	runtimeOnly("com.h2database:h2")
