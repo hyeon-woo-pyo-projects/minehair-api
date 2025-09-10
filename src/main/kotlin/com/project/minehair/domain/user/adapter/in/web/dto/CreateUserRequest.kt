163131285244278cfce99eff43eedf72c0f8e275
woopi1030 <lozenkrantz@gmail.com>
2025-09-10 21:35:44 +0900

    [CHG] 회원가입 시 생년월일 패턴 지정

--- src/main/kotlin/com/project/minehair/domain/user/adapter/in/web/dto/CreateUserRequest.kt
+++ src/main/kotlin/com/project/minehair/domain/user/adapter/in/web/dto/CreateUserRequest.kt
@@ -47,5 +47,9 @@ data class CreateUserRequest(
 
     @Schema(description = "생년월일", example = "1990-01-01", required = true)
     @field:NotNull(message = "생년월일은 필수입니다.")
+    @field:Pattern(
+        regexp = "^\\d{4}-\\d{2}-\\d{2}$",
+        message = "생년월일은 YYYY-MM-DD 형식이어야 합니다."
+    )
     val birthDate: LocalDate
 )
